8d6e6bfa85d36009fe42bec0c17a54bcb421d6aa
BushraTariq1624 <tbushra211@gmail.com>
2025-02-14 04:20:53 -0800

    Backend

---
+++ class2/server/app.mjs
@@ -0,0 +1,14 @@
+
+import express from "express"
+console.log("sdbcakjna");
+const app = express()
+const port = 3000
+//192.168.141.245:3000
+app.get('/', (req, res) => {
+    console.log(req.ip)
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
